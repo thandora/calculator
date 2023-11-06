3f33d3dbff34c4882d89210cc6a9996ec852711c
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2023-11-06 16:29:54 +0800

    Add basic operations functionality

--- script.js
+++ script.js
@@ -25,14 +25,23 @@ function writeInput(char) {
 }
 
 function decodeInput(textDisplay) {
+  // Set global variables operandA, operandB, and operator
+  // according to calc input
   const operands = [];
 
   if (textDisplay.match(/[+\-*\/]/) !== null) {
-    operator = textDisplay.match(/[+\-*\/]/)[0];
-    operands.push(...textDisplay.split(operator));
+    operatorSymbol = textDisplay.match(/[+\-*\/]/)[0];
+    operator = { "+": add, "-": subtract, "*": multiply, "/": divide }[operatorSymbol];
+    operands.push(...textDisplay.split(operatorSymbol));
   } else {
     operands.push(textDisplay);
   }
+
+  console.log(operands);
+  for (let i in operands) {
+    operands[i] = +operands[i];
+  }
+  console.log(operands);
 
   operandA = operands[0];
   if (operands.length > 1) {
@@ -42,17 +51,24 @@ function decodeInput(textDisplay) {
 
 // Screen display
 const dispInput = document.querySelector(".display-input");
-const dispResult = document.querySelector(".display-input");
+const dispResult = document.querySelector(".display-result");
 
-// Operand variables
+// Operation variables
 let operandA;
 let operandB;
 let operator;
+let result;
 
 // Buttons
 const btnEqual = document.querySelector(".operator-func.equal");
 btnEqual.addEventListener("click", () => {
   decodeInput(dispInput.textContent);
+  if (operator === undefined) {
+    result = operandA;
+  } else {
+    result = operate(operandA, operandB, operator);
+  }
+  dispResult.textContent = result;
 });
 
 const btnsDigit = document.querySelectorAll(".digit");
